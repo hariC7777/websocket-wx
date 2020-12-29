f98bbf72e4873b76ccb76bf401ba70c2cde4c665
hariC <harrison_chen0819@live.com>
2020-12-29 16:42:15 +0800

    init

---
+++ src/main/java/com/hari/wsexperiment/WsexperimentApplication.java
@@ -0,0 +1,13 @@
+package com.hari.wsexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsexperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsexperimentApplication.class, args);
+    }
+
+}
